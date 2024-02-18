9fa3690fc90a6097df50e83e06162b8c4b87816a
Respirayson <raysonyeap4c@gmail.com>
2024-02-18 21:24:16 +0800

    Add functionality to parse json requests

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import mountRoutes from "./routes/index.js";
 
 const app = express();
 app.use(cors());
+app.use(express.json()); // For json requests -> good to have but not really needed in the context of this project
 app.use(express.urlencoded({ extended: true })); // POST Req using application/x-www-form-urlencoded
 
 mountRoutes(app);
